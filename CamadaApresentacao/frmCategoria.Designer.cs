c453e02ddfe39056bd5c0b05815619eb2763f6d4
Diego Carvalho <diego.crodrigues8@outlook.com>
2020-07-30 14:54:31 -0300

    crud para negócios

---
+++ CamadaApresentacao/frmCategoria.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace CamadaApresentacao
+{
+    partial class frmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.chkDeletar = new System.Windows.Forms.CheckBox();
+            this.dataLista = new System.Windows.Forms.DataGridView();
+            this.Deletar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtIdCategoria = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorIcone = new System.Windows.Forms.ErrorProvider(this.components);
+            this.ttMensagem = new System.Windows.Forms.ToolTip(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLista)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorIcone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Categorias";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 63);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 375);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblTotal);
+            this.tabPage1.Controls.Add(this.chkDeletar);
+            this.tabPage1.Controls.Add(this.dataLista);
+            this.tabPage1.Controls.Add(this.lblNome);
+            this.tabPage1.Controls.Add(this.txtBuscar);
+            this.tabPage1.Controls.Add(this.btnImprimir);
+            this.tabPage1.Controls.Add(this.btnDeletar);
+            this.tabPage1.Controls.Add(this.btnBuscar);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 346);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Listar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(468, 72);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(40, 17);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Total";
+            // 
+            // chkDeletar
+            // 
+            this.chkDeletar.AutoSize = true;
+            this.chkDeletar.Location = new System.Drawing.Point(21, 71);
+            this.chkDeletar.Name = "chkDeletar";
+            this.chkDeletar.Size = new System.Drawing.Size(76, 21);
+            this.chkDeletar.TabIndex = 6;
+            this.chkDeletar.Text = "Deletar";
+            this.chkDeletar.UseVisualStyleBackColor = true;
+            this.chkDeletar.CheckedChanged += new System.EventHandler(this.chkDeletar_CheckedChanged);
+            // 
+            // dataLista
+            // 
+            this.dataLista.AllowUserToAddRows = false;
+            this.dataLista.AllowUserToDeleteRows = false;
+            this.dataLista.AllowUserToOrderColumns = true;
+            this.dataLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Deletar});
+            this.dataLista.Location = new System.Drawing.Point(21, 98);
+            this.dataLista.MultiSelect = false;
+            this.dataLista.Name = "dataLista";
+            this.dataLista.ReadOnly = true;
+            this.dataLista.RowHeadersWidth = 51;
+            this.dataLista.RowTemplate.Height = 24;
+            this.dataLista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataLista.Size = new System.Drawing.Size(726, 227);
+            this.dataLista.TabIndex = 5;
+            this.dataLista.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataLista_CellContentClick);
+            this.dataLista.DoubleClick += new System.EventHandler(this.dataLista_DoubleClick);
+            // 
+            // Deletar
+            // 
+            this.Deletar.HeaderText = "Deletar";
+            this.Deletar.MinimumWidth = 6;
+            this.Deletar.Name = "Deletar";
+            this.Deletar.ReadOnly = true;
+            this.Deletar.Width = 125;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(18, 27);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(45, 17);
+            this.lblNome.TabIndex = 4;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(70, 24);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(173, 22);
+            this.txtBuscar.TabIndex = 3;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(411, 23);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 2;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(330, 23);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletar.TabIndex = 1;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(249, 23);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 346);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Configuração";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNovo);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.btnEditar);
+            this.groupBox1.Controls.Add(this.btnSalvar);
+            this.groupBox1.Controls.Add(this.txtDescricao);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.txtIdCategoria);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(755, 333);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Categorias";
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(22, 213);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 9;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(347, 213);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(242, 213);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(132, 213);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(113, 123);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescricao.Size = new System.Drawing.Size(268, 64);
+            this.txtDescricao.TabIndex = 5;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(114, 85);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(157, 22);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtIdCategoria
+            // 
+            this.txtIdCategoria.Location = new System.Drawing.Point(113, 47);
+            this.txtIdCategoria.Name = "txtIdCategoria";
+            this.txtIdCategoria.Size = new System.Drawing.Size(158, 22);
+            this.txtIdCategoria.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Descrição";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Código";
+            // 
+            // errorIcone
+            // 
+            this.errorIcone.ContainerControl = this;
+            // 
+            // ttMensagem
+            // 
+            this.ttMensagem.IsBalloon = true;
+            // 
+            // frmCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCategoria";
+            this.Text = "frmCategoria";
+            this.Load += new System.EventHandler(this.frmCategoria_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLista)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorIcone)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.CheckBox chkDeletar;
+        private System.Windows.Forms.DataGridView dataLista;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Deletar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtIdCategoria;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider errorIcone;
+        private System.Windows.Forms.ToolTip ttMensagem;
+    }
+}
---
+++ CamadaApresentacao/frmCategoria.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using CamadaNegocio;
+using CamadaNegocios;
+
+namespace CamadaApresentacao
+{
+    public partial class frmCategoria : Form
+    {
+        private bool eNovo = false;
+        private bool eEditar = false;
+
+        public frmCategoria()
+        {
+            InitializeComponent();
+            this.ttMensagem.SetToolTip(this.txtNome, "Insira o nome da Categoria");
+        }
+
+        //Mostrar mensagem de confirmação
+        private void MensagemOk(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Sistema Comércio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        
+        //Mostrar mensagem de erro
+        private void MensagemErro(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Sistema Comércio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        //Limpar Campos
+        private void Limpar()
+        {
+            this.txtNome.Text = string.Empty;
+            this.txtIdCategoria.Text = string.Empty;
+            this.txtDescricao.Text = string.Empty;
+        }
+
+        //Habilitar os textbox
+        private void Habilitar(bool valor)
+        {
+            this.txtNome.ReadOnly = !valor;
+            this.txtIdCategoria.ReadOnly = !valor;
+            this.txtDescricao.ReadOnly = !valor;
+        }
+
+        //Habilitar os buttons
+        private void Botoes()
+        {
+            if(this.eNovo || this.eEditar)
+            {
+                this.Habilitar(true);
+                this.btnNovo.Enabled = false;
+                this.btnSalvar.Enabled = true;
+                this.btnEditar.Enabled = false;
+                this.btnCancelar.Enabled = true;
+            }
+            else
+            {
+                this.Habilitar(false);
+                this.btnNovo.Enabled = true;
+                this.btnSalvar.Enabled = false;
+                this.btnEditar.Enabled = true;
+                this.btnCancelar.Enabled = false;
+            }
+            
+        }
+
+        //Ocultar as Colunas do Grid
+        private void OcultarColunas()
+        {
+            this.dataLista.Columns[0].Visible = false;
+            this.dataLista.Columns[1].Visible = false;
+        }
+
+
+        //Mostrar no Data Grid
+        private void Mostrar()
+        {
+            this.dataLista.DataSource = NCategoria.Mostrar();
+            this.OcultarColunas();
+            lblTotal.Text = "Total de Registros: " + Convert.ToString(dataLista.Rows.Count);
+        }
+
+        //Buscar pelo nome
+        private void BuscarNome( )
+        {
+            this.dataLista.DataSource = NCategoria.BuscarNome(this.txtBuscar.Text);
+            this.OcultarColunas();
+            lblTotal.Text = "Total de Registros: " + Convert.ToString(dataLista.Rows.Count);
+        }
+
+        private void frmCategoria_Load(object sender, EventArgs e)
+        {
+            this.Top = 0;
+            this.Left = 0;
+            this.Mostrar();
+            this.Habilitar(false);
+            this.Botoes();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            this.BuscarNome();
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            this.BuscarNome();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            this.eNovo = true;
+            this.eEditar = false;
+            this.Botoes();
+            this.Limpar();
+            this.Habilitar(true);
+            this.txtNome.Focus();
+            this.txtIdCategoria.Enabled = false;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string resp = "";
+                if(this.txtNome.Text == string.Empty)
+                {
+                    MensagemErro("Preencha todos os campos");
+                    errorIcone.SetError(txtNome, "Insira o nome");
+                }
+                else
+                {
+                    if (this.eNovo)
+                    {
+                        resp = NCategoria.Inserir(txtNome.Text.Trim().ToUpper(), 
+                            txtDescricao.Text.Trim());
+                    }
+                    else
+                    {
+                        resp = NCategoria.Editar(Convert.ToInt32(txtIdCategoria.Text), 
+                            txtNome.Text.Trim().ToUpper(), txtDescricao.Text.Trim());
+                    }
+
+                    if (resp.Equals("Ok"))
+                    {
+                        if (this.eNovo)
+                        {
+                            this.MensagemOk("Registro salvo com sucesso!");
+                        }
+                        else
+                        {
+                            this.MensagemOk("Registro editado com sucesso!");
+                        }
+                    }
+                    else
+                    {
+                        this.MensagemErro(resp);
+                    }
+
+                    this.eNovo = false;
+                    this.eEditar = false;
+                    this.Botoes();
+                    this.Limpar();
+                    this.Mostrar();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void dataLista_DoubleClick(object sender, EventArgs e)
+        {
+            this.txtIdCategoria.Text = Convert.ToString(this.dataLista.CurrentRow.Cells["idcategoria"].Value);
+            this.txtNome.Text = Convert.ToString(this.dataLista.CurrentRow.Cells["nome"].Value);
+            this.txtDescricao.Text = Convert.ToString(this.dataLista.CurrentRow.Cells["descricao"].Value);
+            this.tabControl1.SelectedIndex = 1;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (this.txtIdCategoria.Text.Equals(""))
+            {
+                this.MensagemErro("Selecione um registro para inserir");
+            }
+            else
+            {
+                this.eEditar = true;
+                this.Botoes();
+                this.Habilitar(true);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.eNovo = false;
+            this.eEditar = false;
+            this.Botoes();
+            this.Habilitar(false);
+            this.Limpar();
+        }
+
+        private void chkDeletar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkDeletar.Checked)
+            {
+                this.dataLista.Columns[0].Visible = true;
+            }
+            else
+            {
+                this.dataLista.Columns[0].Visible = false;
+            }
+        }
+
+        private void dataLista_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataLista.Columns["Deletar"].Index)
+            {
+                DataGridViewCheckBoxCell ChkDeletar = (DataGridViewCheckBoxCell)dataLista.Rows[e.RowIndex].Cells["Deletar"];
+                ChkDeletar.Value = !Convert.ToBoolean(ChkDeletar.Value);
+            }
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult Opcao;
+                Opcao = MessageBox.Show("Realmente Deseja apagar os Registros?", "Sistema Comércio", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if(Opcao == DialogResult.OK)
+                {
+                    string Codigo;
+                    string Resp = "";
+
+                    foreach(DataGridViewRow row in dataLista.Rows)
+                    {
+                        if (Convert.ToBoolean(row.Cells[0].Value))
+                        {
+                            Codigo = Convert.ToString(row.Cells[1].Value);
+                            Resp = NCategoria.Excluir(Convert.ToInt32(Codigo));
+
+                            if (Resp.Equals("Ok"))
+                            {
+                                this.MensagemOk("Registro excluido com sucesso");
+                            }
+                            else
+                            {
+                                this.MensagemErro(Resp);
+                            }
+                        }
+                    }
+                    this.Mostrar();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+    }
+}
